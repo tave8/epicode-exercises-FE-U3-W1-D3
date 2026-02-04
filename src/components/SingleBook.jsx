ac3f35d4773acc58ff82025cd302778229fd938f
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-04 14:40:57 +0100

    add selected state to SingleBook

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,15 +1,28 @@
+import { Component } from "react"
 import { Card } from "react-bootstrap"
 
-const SingleBook = (props) => {
-  const { book } = props
-  return (
-    <Card style={{ width: "18rem", height: "500px" }} key={book.asin}>
-      <Card.Img variant="top" src={book.img} />
-      <Card.Body>
-        <Card.Title>{book.title}</Card.Title>
-      </Card.Body>
-    </Card>
-  )
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  }
+
+  toggleSelected() {
+    this.setState({
+      selected: !this.state.selected,
+    })
+  }
+
+  render() {
+    const { book } = this.props
+    return (
+      <Card style={{ width: "18rem", height: "500px" }} key={book.asin} onClick={this.toggleSelected.bind(this)}>
+        <Card.Img variant="top" src={book.img} />
+        <Card.Body>
+          <Card.Title>{book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    )
+  }
 }
 
 export default SingleBook
